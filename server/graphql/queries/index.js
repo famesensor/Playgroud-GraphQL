9df66c1f786c88273fd4a98a5c639c8fca1a174d
famesensor <famfew25@gmail.com>
2020-10-13 00:23:14 +0700

    [update]: import mode, lib

---
+++ server/graphql/queries/index.js
@@ -0,0 +1,5 @@
+const { GraphQLList, GraphQLObjectType } = require('graphql');
+const User = require('../../model/user');
+const Post = require('../../model/post');
+const UserType = require('./userType');
+const PostType = require('./postType');
