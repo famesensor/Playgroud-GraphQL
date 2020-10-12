1190c52b6b496b21bd429e95d33e2a67ecd6a1b4
famesensor <famfew25@gmail.com>
2020-10-13 00:23:57 +0700

    :tada: init commit user model

---
+++ server/model/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('user', userSchema);
