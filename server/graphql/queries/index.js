2b292f65b74c3949c488881192d696d68a2b1d7e
famesensor <famfew25@gmail.com>
2020-10-14 01:20:15 +0700

    :sparkles: queryroot function

--- server/graphql/queries/index.js
+++ server/graphql/queries/index.js
@@ -3,3 +3,26 @@ const User = require('../../model/user');
 const Post = require('../../model/post');
 const UserType = require('./userType');
 const PostType = require('./postType');
+
+const QueryRootType = new GraphQLObjectType({
+    name: 'WebAppSchema',
+    description: 'Web Application query root',
+    fields: () => ({
+        authors: {
+            type: GraphQLList(UserType),
+            description: 'List of all Authors',
+            resolve: async () => {
+                console.log('find user!');
+                return await User.find().exec();
+            }
+        },
+        posts: {
+            type: GraphQLList(PostType),
+            description: 'List of all Posts',
+            resolve: async () => {
+                console.log('find posts!');
+                return await Post.find().exec();
+            }
+        }
+    })
+});
