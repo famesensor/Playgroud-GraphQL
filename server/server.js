759c2d50a201b09efa9bf0b2969fe60a478f5a70
famesensor <famfew25@gmail.com>
2020-10-15 01:08:26 +0700

    [update]: import schema, graphqlhttp, add middleware graphql

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
 require('./config/index');
-const express = require('express');
-const cors = require('cors');
+const express = require('express'),
+    cors = require('cors'),
+    { graphqlHTTP } = require('express-graphql'),
+    schema = require('./graphql/schema');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: schema,
+        graphiql: true
+    })
+);
 
 const port = 5000 || process.env.port;
 
